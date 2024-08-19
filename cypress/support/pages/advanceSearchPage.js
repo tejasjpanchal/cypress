6b45271306b102eb6a30462eb45b6c2ffb88c91c
tejasjpanchal <tejasjpanchal@gmail.com>
2024-08-19 11:01:13 +0200

    added the feature to select date by the Datepicker

--- cypress/support/pages/advanceSearchPage.js
+++ cypress/support/pages/advanceSearchPage.js
@@ -1,4 +1,5 @@
 import { getIndexValue } from '../utilityFunction.js';
+import "cypress-real-events";
 
 class advanceSearchPage {
 
@@ -6,8 +7,8 @@ class advanceSearchPage {
     defaultSearchSelector      =  '[data-testid="chip-list-test-id"]'
     searchFilterSelector       =  '[data-testid="accordion-item"]'
     birthdayFilterSelector     =  'input[data-testid="birthday-input-test-id"]'
-    seeResultButtonSelector    =  '[data-testid="adv-search-get-results"]'
-    nameSelector               =  '[data-testid="nlib-title"]'
+    seeResultButtonSelector    =  '[data-testid="adv-search-get-results"]'    
+    nameSelector               =  '.ipc-title a'
     startDateSelector          =  '[data-testid="birthDate-start"]'
     endDateSelector            =  '[data-testid="birthYearMonth-end"]'
 
@@ -19,7 +20,7 @@ class advanceSearchPage {
 
     }
 
-        
+    
     // Method    : To unfold specific Search Filter based on Given Name under Advance Search 
     // Parameter : filterName
     unfoldSearchFilterByGivenName(filterName) {
@@ -27,6 +28,51 @@ class advanceSearchPage {
         cy.get(this.searchFilterSelector).contains(filterName).click();
 
     }    
+
+    // Method    : To select starrt date using date picker 
+    selectStartDateSameAsToday40YearsAgoUsingDatepicker() {        
+            
+        cy.get(this.startDateSelector).click({ force: true });
+        cy.wait(1000);
+        
+        // Opening Datpicker using F4
+        cy.get(this.startDateSelector).realPress("F4");
+
+        // Pressing TAB 3 time to go to Year selction section on datepicker
+        cy.realPress("Tab");
+        cy.realPress("Tab");
+        cy.realPress("Tab");
+        cy.wait(1000);  // wait to complete action to make sure next action get sufficient time to perform as action as expcted  
+
+        cy.realPress("Enter"); // to open year selction section 
+        cy.wait(1000) // wait to complete action to make sure next action get sufficient time to perform as action as expcted  
+        
+        // Loop through 40 times to go back 40 years behind
+        for (let i = 1; i <= 40; i++) {
+            cy.realPress("PageUp");
+            cy.wait(500);
+        }
+        
+        // To select found date
+        cy.realPress("Enter");
+        cy.realPress("Enter");
+    }
+    
+    
+    // Method    : To Enter date in to Date string field of Birth date filter  
+    // Parameter : Birthday
+    enterDateInToDateStringField(Birthday) {
+        
+        cy.get(this.endDateSelector).type(`${Birthday}{enter}`);        
+        
+    }
+
+    // Method    : To click on See Result button    
+    clickOnSeeResultButton() {        
+
+        cy.get(this.seeResultButtonSelector).contains('See results').click({force: true}); 
+        
+    }
 
     // Method    : To Search Name by Birthday  
     // Parameter : Birthday
@@ -37,13 +83,14 @@ class advanceSearchPage {
         cy.get(this.seeResultButtonSelector).contains('See results').click({force: true}); 
         
     }
+   
 
     // Method    : To search result by entering the date with format (YYYY-MM-DD) under from date filed and 
     //             entering the date with format (YYYY-MM) in the string field for the “to” option  
     // Parameter : Birthday
     searchByBirthDate(BirthDate) {
         
-        cy.get(this.startDateSelector).type(BirthDate)       
+        //cy.get(this.startDateSelector).type(BirthDate)       
         
         cy.get(this.endDateSelector).type(BirthDate)    
 
@@ -58,7 +105,7 @@ class advanceSearchPage {
         // Getting nth Name Index value which is one less than actual value
         let indexValue  = getIndexValue(nthName);
 
-        cy.get(this.nameSelector).eq(indexValue).click();        
+        cy.get(this.nameSelector).eq(indexValue).click({force: true});        
 
     }    
 
